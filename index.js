04925e195b4a1f64133a31c13a65af19b0f93976
mhrdwan <futuremhr@gmail.com>
2023-07-13 14:55:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { default: axios } = require('axios');
+const { ThreadsAPI } = require('threads-api');
+
+let lokasi = "ciracas"
+const Api = async () => {
+    const api = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.APIKEY}&q=${lokasi}&aqi=yes`)
+    // console.log(api.data);
+    const IsiChat = (`˜”*°•.˜”*°• Update Cuaca Ciracas •°*”˜.•°*”˜\nLokasi : ${api.data.location.name}\nNegara : ${api.data.location.country}\nSuhu : ${api.data.current.feelslike_c}°C\nTerakhir Update : ${api.data.current.last_updated}\n\n\nUpdate Bot Per 1 Jam\nᴮʸ ᴿⁱᵈʷᵃⁿ`)
+    console.log(`˜”*°•.˜”*°• Update Cuaca Ciracas •°*”˜.•°*”˜\nLokasi : ${api.data.location.name}\nNegara : ${api.data.location.country}\nSuhu : ${api.data.current.feelslike_c}°C\nTerakhir Update : ${api.data.current.last_updated}\n\n\nUpdate Bot Per 1 Jam\nᴮʸ ᴿⁱᵈʷᵃⁿ`);
+    return IsiChat
+}
+
+
+const main = async () => {
+    const threadsAPI = new ThreadsAPI({
+        username: process.env.UNAME,
+        password: process.env.PASSW,
+    });
+
+    await threadsAPI.publish({
+        text: await Api(),
+    });
+    console.log("Sukses");
+};
+
+const Kumpul = async () => {
+    setInterval(async() => {
+        await Api()
+        await main();
+        
+    }, 5000);
+}
+Kumpul()
